4756816ec2607e00fe5440c21d3e4ef3cbe4a427
tiramisa <miller19988@gmail.com>
2023-06-15 18:52:46 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -17,10 +17,6 @@ function toggleColorSwitch() {
   }
 }
 
-function changeBackgroundColor() {
-  document.body.style.backgroundColor = getRandomHexColor();
-}
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
